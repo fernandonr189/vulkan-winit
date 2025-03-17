7798ee6b58fae5d4decfd841619d956e195c0fcb
Fernando Ismael Navarrete Ruiz <fernandonr189@outlook.com>
2025-03-16 18:39:35 -0600

    Added winit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod util;
+use util::app::App;
+use winit::event_loop::{ControlFlow, EventLoop};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = App::default();
+    let _ = event_loop.run_app(&mut app);
 }
